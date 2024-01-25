8008c9c1026b0f7470a74759c52674650659865e
SEOpsuj <jantarnawa515@gmail.com>
2024-01-25 09:19:34 +0000

    studentmachine send

--- cpp/gulasz/main.cpp
+++ cpp/gulasz/main.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
-	return 0;
+int km=0;
+int spal=0;
+int cena=0;
+int osoby=0;
+int wynik=0;
+cout<<"trasa w km ";
+cin>>km;
+cout<<"średnie spalanie na 100km ";
+cin>>spal;
+cout<<"cena za litr ";
+cin>>cena;
+cout<<"ilość osób ";
+cin>>osoby;
+wynik=km/100;
+wynik=wynik*spal;
+wynik=wynik*cena;
+wynik=wynik/osoby;
+cout<<wynik;
 }
